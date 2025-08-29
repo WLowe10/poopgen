f81ddc65c8d7b750e6a56d6391ce9740cbc69428
Wes Lowe <wesley.jlowe10@gmail.com>
2025-08-29 15:21:30 -0500

    added unit test for parseDirectory

---
+++ src/parse.test.ts
@@ -0,0 +1,37 @@
+import { vi, beforeEach, it, expect } from "vitest";
+import { vol } from "memfs";
+import { parseDirectory } from "./parse";
+
+vi.mock("node:fs", async () => {
+	const { fs } = await import("memfs");
+
+	return {
+		default: fs,
+	};
+});
+
+vi.mock("node:fs/promises", async () => {
+	const { fs } = await import("memfs");
+
+	return {
+		default: fs.promises,
+	};
+});
+
+beforeEach(() => {
+	vol.reset();
+});
+
+it("can parse a template using all features", async () => {
+	vol.fromJSON({
+		"./template/test.txt": "test",
+		"./template/test2.txt.ejs": "test",
+		"./template/[package.json]": "escaped",
+		"./template/_poop.js": "export async function before() {}",
+		"./template/nested/nested.txt": "nested",
+	});
+
+	const entries = await parseDirectory("template");
+
+	expect(entries).toMatchSnapshot();
+});
